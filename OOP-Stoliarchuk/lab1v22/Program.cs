82e87d9649c806a2723a685853ecf988e27607a7
ddisskrri52 <stoliarchuk.d.v_ipz24@rcit.ukr.education>
2026-09-09 23:07:38 +0300

    Add lab 1

---
+++ OOP-Stoliarchuk/lab1v22/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace lab1v2
+{
+    class Car
+    {
+        private string brand;
+        private string model;
+
+       public int Year { get; set; }
+
+       public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public Car(string brand, string model, int year)
+        {
+            this.brand = brand;
+            this.model = model;
+            Year = year;
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine($"Автомобіль {Brand} {Model} ({Year} року) вирушив у дорогу!");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Toyota", "Camry", 2020);
+            Car car2 = new Car("BMW", "M5", 2022);
+            Car car3 = new Car("Tesla", "Model 3", 2023);
+
+            car1.Drive();
+            car2.Drive();
+            car3.Drive();
+        }
+    }
+}
